aece432c95d7daf6adf6b53c89fd9d8450d4465a
Cauã Ferreira <caualessaferreira@gmail.com>
2025-10-17 22:12:04 -0300

    Add files via upload

---
+++ alarme.py
@@ -0,0 +1,21 @@
+import random
+from playsound3 import playsound
+import os.path
+import time
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+filename = os.path.join(CURRENT_DIRECTORY, "Assets/alarme.mp3")
+
+sorte = int(input("Digite um numero de 0 a 100 para escolher a chance"))
+atocado = False
+alarme = int(random.randrange(0, 100))
+
+while atocado == False:
+    if sorte >= alarme:
+        playsound(filename)
+        print("Alarme Tocou!")
+        atocado = True
+
+        break
+    time.sleep(1)
+
